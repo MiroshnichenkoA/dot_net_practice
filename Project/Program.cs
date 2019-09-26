b61256be650031772a982bcae42de16f47aa34a6
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-09-26 20:36:25 +0300

    project

---
+++ Project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.TimeSpan t1 = DateTime.Now.TimeOfDay;
+
+            int h = t1.Hours;
+                int h1 = (int)h;
+
+            if (h1 >= 9 && h1 < 12) Console.WriteLine("Good morning,guys!");
+            else if (h1 >= 12 && h1 < 15) Console.WriteLine("Good day,guys!");
+            else if (h1 >= 15 && h1 < 22) Console.WriteLine("Good evening,guys!");
+            else Console.WriteLine("Good night,guys!");
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
