ac01a47bc8500cd8efd5bb0d18994d3738b6a8bf
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-10-03 19:49:41 +0300

    task2

--- Project3/Project/Program.cs
+++ Project3/Project/Program.cs
@@ -6,11 +6,27 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            CreateEmptyMussive();
+            int[] firstmassive = CreateEmptyMussive();
+            string[] = CreateNewMussive();
         }
         static void CreateEmptyMussive(string[] args)
         {
             int[] emptyMassive = { }; //массив заполнится дефолтными значениями 0, чисто технически он не пустой, но математически вроде и пустой
+        }
+        static void CreateNewMussive(string[] args)
+        {
+            string[] newMassive = new string[3];
+            int number = 32;
+            string txt = "Hello";
+            newMassive[0] = Convert.ToString(number);
+            newMassive[1] = Convert.ToString('\u0041');
+            newMassive[2] = "txt";
+            Console.WriteLine(newMassive[0], newMassive[1], newMassive[2]);
+
+            number = number + 10;
+            newMassive[0] = Convert.ToString(number);
+            newMassive[2] = "txt, guys!";
+            Console.WriteLine(newMassive[0], newMassive[1], newMassive[2]);
         }
     }
 }
