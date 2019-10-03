1db0a8cba787f941c834328255098a559421ddde
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-10-03 19:38:42 +0300

    task1

---
+++ Project3/Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateEmptyMussive();
+        }
+        static void CreateEmptyMussive(string[] args)
+        {
+            int[] emptyMassive = { }; //массив заполнится дефолтными значениями 0, чисто технически он не пустой, но математически вроде и пустой
+        }
+    }
+}
