3ae29f7e023e9d783293670646b26fd0ad2fc420
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-10-24 20:30:29 +0300

    second task

---
+++ lecture_13/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_13
+{
+   static class StringExtensions
+    {
+        public static void CutString(this String string)
+        {
+            string new = string.
+        }
+    }
+}
