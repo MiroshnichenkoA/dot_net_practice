dc593f919fe9c17c5ac311bfe4286525e3e52209
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-10-24 20:23:01 +0300

    first task

---
+++ lecture_13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lecture_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's create a song! Please, enter the Title, song length in minutes, Author's name and Album year:");
+            string title = Console.ReadLine();
+            int minutes = int.Parse(Console.ReadLine());
+            string author = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            Song mySong = new Song(title, minutes, author, year);
+            var data = GetSongData(mySong);
+            Console.WriteLine(data);
+        }
+        
+        public static var GetSongData(Song song)
+        {
+            var data = new
+                {
+                Title = song.Title, 
+                Minutes = song.Minutes, 
+                Year = song.Year
+                };
+            return data;
+        }
+    }
+}
